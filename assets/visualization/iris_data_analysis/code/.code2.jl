fdfe653ba6bc0310671fc8d6bd35e19ad356880c
JunDamin <freedomgod@gmail.com>
2021-07-14 19:08:31 +0000

    Deploying to gh-pages from  @ 73cfb09e6d6a1c252973712e8e0a1f7b6a7cb9d0 🚀

---
+++ assets/visualization/iris_data_analysis/code/.code2.jl
@@ -0,0 +1,16 @@
+# This file was generated, do not modify it. # hide
+p = data |> 
+    @vlplot(
+        repeat={
+            row=[:sepalLength,:sepalWidth,:petalLength, :petalWidth], 
+            column=[:sepalLength,:sepalWidth,:petalLength, :petalWidth]
+        }
+    ) + 
+    @vlplot(
+        :point, 
+        x={field={repeat=:column}, type=:quantitative},
+        y={field={repeat=:row}, type=:quantitative},
+        color={field=:species},
+    )
+
+save(joinpath(@OUTPUT, "plot2.svg"), p)  # hide
---
+++ assets/visualization/iris_data_analysis/code/.iris_read.jl
@@ -0,0 +1,4 @@
+# This file was generated, do not modify it. # hide
+using VegaDatasets
+data = dataset("iris")
+print(data)
